6f7a1f0021f24d5a79317074d018e5ec27ba1968
Felix/failaxite <failaxite.don@gmail.com>
2023-03-27 11:42:14 +0200

    testing page and style

--- index.php
+++ index.php
@@ -1,14 +1,50 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/style.css">
-    <title>Document</title>
+	<title>Feedback</title>
+	<link rel="stylesheet" type="text/css" href="public/css/output.css">
 </head>
-<body class="index">
-    <h1>Page index</h1>
-    
+<body>
+	<header>
+		<div class="logo"></div>
+		<nav>
+			<ul>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Demandes</a></li>
+				<li><a href="#">Accessibilité</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<h1>Nous contacter</h1>
+		<p>Nous apprécions votre opinion ! N'hésitez pas à partager votre expérience avec nous !</p>
+		<div class="form">
+            <div class="right">
+			<h3>Ecrivez votre retour</h3>
+			<textarea placeholder="Votre message"></textarea>
+            </div>
+			<div class="left">
+				<h3>Selectionner un établissement</h3>
+				<textarea placeholder="Votre sélection"></textarea>
+				<h3>Votre nom</h3>
+				<textarea placeholder="Votre nom"></textarea>
+			</div>
+                <button>Envoyer</button>
+		</div>
+	</main>
+	<footer>
+		<div class="left-footer">
+			<h3>Place de l'Hotel de Ville</h3>
+			<p>BP 60086 92161 Antony Cedex</p>
+		</div>
+		<div class="middle">
+			<h3>Gestion de cookies</h3>
+			<p>Mentions légales</p>
+		</div>
+		<div class="right">
+			<h3>TEL: 01 40 96 71 00</h3>
+			<p><u>CONTACT PAR EMAIL</u></p>
+		</div>
+	</footer>
 </body>
 </html>
