98f1608f927b0d5fb6a9bb64cc79378ede278809
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-03-28 17:40:39 +0200

    :sparkles: modif header + debut page accueil

--- index.php
+++ index.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/output.css">
+    <link rel="stylesheet" href="public/css/pages/accueil.css">
     <title>Document</title>
 </head>
 <body class="index">
     <?php
     require_once 'header.php';
     ?>
-    <h1>Page index</h1>
+    <div class ="accueil-content">
+        <img src="images/fond.png" alt="fond">
+        <div class="accueil-text">
+            <h1>Bienvenue sur Antony Accessibilité</h1>
+            <h2>La carte qui montre où tous les accès sont possibles !</h2>
+        </div>
+    </div>
     
     <script src="public/js/script.js"></script>
 </body>
