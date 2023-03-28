a1122ddb81a31013080e351c5a09bd6a7bafe28e
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-03-28 15:47:41 +0200

    :sparkles: add header responsive

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     <title>Document</title>
 </head>
 <body class="index">
+    <?php
+    require_once 'header.php';
+    ?>
     <h1>Page index</h1>
     
+    <script src="public/js/script.js"></script>
 </body>
 </html>
