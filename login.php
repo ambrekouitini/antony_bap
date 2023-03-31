15466ad500af91efe674e19f6d08da6648ccbb25
Ambre Kouitini <ambrektn@outlook.fr>
2023-03-31 11:25:26 +0200

    :lipstick: Style of login page

--- login.php
+++ login.php
@@ -35,27 +35,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Changa:wght@200;300;400;500;600;700;800&family=Press+Start+2P&display=swap" rel="stylesheet">
     <title>Connexion</title>
 </head>
-<body>
-    <h1>Connexion</h1>
+<body class="login">
 
-    <form method="POST">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email">
+    <div class="containerLeft">
+        <div class="title">
+            <img src="/images/antonyLogo.png" alt="logo ville antony">
+            <h1>Access'Tony</h1>
+        </div>
+    </div>
+    <div class="containerRight">
+        <div class="mainContainer">
+            <h2>Connexion</h2>
+            <p>Connectez-vous pour accéder à votre espace admin.</p>
+        </div>
+        <div class="formContainer">
+            <form method="POST">
+                <div class="labelContainer">
+                    <!-- <label for="email">Email</label> -->
+                    <input type="email" name="email" id="email" placeholder="adresse mail">
+                </div>
+                <div class="labelContainer">
+                    <!-- <label for="password">Mot de passe</label> -->
+                    <input type="password" name="password" id="password" placeholder="mot de passe">
+                </div>
 
-        <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password">
+                <?php if(isset($_GET['error']) && $_GET['error'] == 1) { ?>
+                    <div class="errorLog">
+                        <h3>Mot de passe ou email incorrect !</h3>
+                        <p>Entrez un mot de passe et un email correspondant</p>
+                    </div>
+                <?php } ?>
 
-        <?php if(isset($_GET['error']) && $_GET['error'] == 1) { ?>
-            <div class="errorLog">
-                <h3>Mot de passe ou email incorrect !</h3>
-                <p>Entrez un mot de passe et un email correspondant</p>
-            </div>
-        <?php } ?>
+                <button type="submit">Se connecter</button>
+            </form>
+        </div>
+    </div>
 
-        <button type="submit">Se connecter</button>
-    </form>
+
+
     
 </body>
 </html>
