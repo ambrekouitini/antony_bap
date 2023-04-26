ee91ca70e05918ba0f9686721bd7e58750e1857f
BARDAT Ryan <105910237+REIG3N@users.noreply.github.com>
2023-04-26 16:03:44 +0200

    : lisptick: Footer adjustement

--- footer.php
+++ footer.php
@@ -1,16 +1,22 @@
 <footer>
     <section>
-        <div class="footerSection1">
-            <p class="footerSectionFirstLine">Place de l'Hôtel de ville</p>
-            <p>BP 60086 92161 Antony Cedex
+        <div class="footerClient">
+            <div class="footerSection1">
+                <p class="footerSectionFirstLine">Place de l'Hôtel de ville</p>
+                <p>BP 60086 92161 Antony Cedex</p>
+            </div>
+            <div class="footerSection2">
+                <a class="footerSectionFirstLine" href="#">Gestion des cookies</a>
+                <a href="#" >Mentions légales</a>
+            </div>
+            <div class="footerSection3">
+                <p class="footerSectionFirstLine">TEL : 01 40 96 71 00</p>
+                <a class="" href="#">CONTACT PAR EMAIL</a>
+            </div>
         </div>
-        <div class="footerSection2">
-            <a class="footerSectionFirstLine" href="#">Gestion des cookies</a>
-            <a href="#" >Mentions légales</a>
-        </div>
-        <div class="footerSection3">
-            <p class="footerSectionFirstLine">TEL : 01 40 96 71 00</p>
-            <a href="#">CONTACT PAR EMAIL</a>
+        <div  class="AdminAccess">
+            <a href="#">Accès Admin</a>
         </div>
     </section>
+
 </footer>
